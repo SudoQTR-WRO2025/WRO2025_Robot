45f87fbe919f5f09505b1366d8c046d5624254a5
SudoQTR-WRO2025 <sudoqtr@gmail.com>
2025-09-27 04:13:09 +0300

    Update steering_test.ino

--- code/arduino/steering_test.ino
+++ code/arduino/steering_test.ino
@@ -1,1 +1,21 @@
+// In this code, the servo (steering system) is simply being tested to make sure the servo can move freely and smoothly in its full range.
+#include <Servo.h>
 
+Servo myservo;
+
+int pos = 0;
+
+void setup() {
+  myservo.attach(9);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);              
+    delay(15);                    
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { 
+    myservo.write(pos);              
+    delay(15);                      
+  }
+}
